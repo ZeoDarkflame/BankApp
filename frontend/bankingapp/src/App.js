464d89b7a282b9b3081603d15dbe6390ac640b3e
Rishi_696 <rishi102001@gmail.com>
2023-08-10 16:29:21 +0530

    Login Page and Routing

--- frontend/bankingapp/src/App.js
+++ frontend/bankingapp/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
+import {BrowserRouter as Router, Route,Routes, Link} from 'react-router-dom'
 import './App.css';
-
+import LoginForm from './components/LoginValidation/LoginForm';
+import Dashboard from './components/Dashboard'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>BankingApp</h1>
+      <Router>
+
+        <div className="container">
+        <Routes>
+          <Route path = "/dashboard" element = {<Dashboard/>}/>
+          <Route path = "/login" element = {<LoginForm/>}/>  
+        </Routes>
+            
+       
+         
+    
+        </div>
+    
+      
+      </Router>
     </div>
   );
 }
---
+++ frontend/bankingapp/src/components/LoginValidation/LoginForm.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import validation from './Validation'
+function LoginForm()
+{
+    const [values, setValues] = useState({
+        name: '',
+        password: ''
+    })
+
+    const [error,setError] = useState({})
+
+    function handleChange(e){
+        setValues({...values,[e.target.name]:e.target.value})
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        setError(validation(values));
+    }
+    return(
+        <div classname='wrap'>
+            <form onSubmit={handleSubmit}>
+                <div className="container">
+                    <label><b>UserName</b></label>
+                    <input type="text" placeholder="Enter Username" value={values.name} name="name" onChange={handleChange}/>
+                    {error.name && <p style={{color:"red", fontSize:"13px"}}>{error.name}</p>}
+                    <label><b>Password</b></label>
+                    <input type="password" placeholder="Enter Password" value={values.password} name="password" onChange={handleChange}/>
+                    {error.password && <p style={{color:"red", fontSize:"13px"}}>{error.password}</p>}
+
+                    
+                    <button type="submit">Login</button>
+                    <input type="checkbox" checked="checked" /> Remember me
+
+                    <div className="container" style={{backgroundColor:"#f1f1f1"}}>
+                    <button type="button" className="cancelbtn">Cancel</button>
+                    <span className="psw">Forgot <a href="#">password</a></span>
+                    </div>
+
+
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm;
