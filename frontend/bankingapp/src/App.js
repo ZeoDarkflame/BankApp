5db41aee4a38e07ef2e495b0bee88a359fd3d78f
Rishi_696 <rishi102001@gmail.com>
2023-08-11 09:53:17 +0530

    Home page

--- frontend/bankingapp/src/App.js
+++ frontend/bankingapp/src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
-import {BrowserRouter as Router, Route,Routes, Link} from 'react-router-dom'
+import React from "react";
+import Navbar from "./components/navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Route,Routes, Link,Switch} from 'react-router-dom'
 import './App.css';
 import LoginForm from './components/LoginValidation/LoginForm';
 import Dashboard from './components/Dashboard'
@@ -10,10 +13,30 @@ function App() {
       <Router>
 
         <div className="container">
-        <Routes>
-          <Route path = "/dashboard" element = {<Dashboard/>}/>
-          <Route path = "/login" element = {<LoginForm/>}/>  
-        </Routes>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/signup">Sign Up</Link>
+              </li>
+              <li>
+                <Link to="/login">Admin</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/login" element={<LoginForm/>}/>
+          </Routes>
+    
+          {/* <Route path = "/dashboard" element = {<Dashboard/>}/>
+          <Route path = "/login" element = {<LoginForm/>}/>   */}
+
             
        
          
