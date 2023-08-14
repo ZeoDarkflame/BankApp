bca75b63e1fdece8bbc592217b35c14125d5d99f
AbhishekNarayan <abhisheknarayan354@gmail.com>
2023-08-14 16:59:06 +0530

    AcountController

--- backend/springapp/src/main/java/com/bankingapp/controllers/AccountController.java
+++ backend/springapp/src/main/java/com/bankingapp/controllers/AccountController.java
@@ -1,12 +1,18 @@
 package com.bankingapp.controllers;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import com.bankingapp.service.AccountService;
+
+import jakarta.validation.Valid;
+
 import com.bankingapp.exceptions.ResourceNotFoundException;
 import com.bankingapp.models.Account;
 import com.bankingapp.models.Customer;
@@ -19,7 +25,7 @@ public class AccountController {
 	@Autowired 
 	private AccountService accountService;
 	
-	@GetMapping("/readall")
+	@GetMapping(path = "/accounts", produces = {MediaType.APPLICATION_JSON_VALUE})
 	public List<Account> read() {
 		return accountService.getAccountFromDatabase();
 	}
@@ -33,14 +39,23 @@ public class AccountController {
 		return account;  
 	}  
 	
-	@PostMapping("/add")
-	public String add() {
-		Account account = new Account();
-		accountService.addAccountInDatabase(account);
-		return "Account added successfully";
-	}
-	
-	
+	@PostMapping("/accounts")
+    public Account createAccount(@Valid @RequestBody Account newAccount) {
+        return accountService.createAccount(newAccount);
+    }
+    
+    @PutMapping("/account/{id}")
+    public ResponseEntity<Account> updateAccount(@PathVariable(value = "id")  Integer account_id, @Valid @RequestBody Account newAccount) throws ResourceNotFoundException 
+    {
+        return accountService.updateAccount(account_id, newAccount);
+    }
+    
+
+    @DeleteMapping("/accounts/{id}")
+    public Map<String,Boolean> deleteAccount(@PathVariable ("id") int accountId) throws ResourceNotFoundException
+    {
+    	return accountService.deleteAccount(accountId);
+    }
 }
 
 
