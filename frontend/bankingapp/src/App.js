9bf689f71e3a4e8cf857ae896ac00bf7a7948ed1
Rishi_696 <rishi102001@gmail.com>
2023-08-14 15:07:20 +0530

    Front end new

--- frontend/bankingapp/src/App.js
+++ frontend/bankingapp/src/App.js
@@ -1,61 +1,42 @@
-import logo from './logo.svg';
-import React from "react";
-import Navbar from "./components/navbar";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router, Route,Routes, Link,Switch} from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
-import LoginForm from './components/LoginValidation/LoginForm';
-import Dashboard from './components/Dashboard'
-import Register from './components/Register'
+import Login from './components/Login.js';
+import './components/Login.css';
+import Signup from './components/Signup.js';
+import './components/Signup.css';
+import Dashboard from './components/Dashboard.js';
+import './components/Dashboard.css';
+import AdminDashboard from './components/Admindashboard';
+import AdminLogin from './components/Adminlogin';
+import './components/Admindashboard.css';
+import './components/Adminlogin.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>BankingApp</h1>
-      <Router>
-
-        <div className="container">
-<<<<<<< HEAD
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-              <li>
-                <Link to="/signup">Sign Up</Link>
-              </li>
-              <li>
-                <Link to="/login">Admin</Link>
-              </li>
-            </ul>
-          </nav>
-
-          <Routes>
-            <Route path="/login" element={<LoginForm/>}/>
-          </Routes>
-    
-          {/* <Route path = "/dashboard" element = {<Dashboard/>}/>
-          <Route path = "/login" element = {<LoginForm/>}/>   */}
-
-=======
-        <Routes>
-          <Route path = "/dashboard" element = {<Dashboard/>}/>
-          <Route path = "/login" element = {<LoginForm/>}/>  
-          <Route path = "/register" element = {<Register/>}/>  
-        </Routes>
->>>>>>> f1671566fa1ff7295c9f599b07761cbf8b22f68d
+    <Router>
+      <div className="App">
+        <nav className="navbar">
+          <ul className="nav-list">
             
-       
-         
-    
-        </div>
-    
-      
-      </Router>
-    </div>
+            <li className="nav-item"><Link to="/login">Login</Link></li>
+            <li className="nav-item"><Link to="/signup">Signup</Link></li>
+            <li className="nav-item"><Link to="/admin"> Admin</Link></li>
+          </ul>
+          <h1>Welcome to Wells Fargo Net Banking</h1>
+        </nav>
+        <Routes>
+        
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/admindashboard" element={<AdminDashboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
