70a967d34a36b5841681f03ca3d441faf2b50de6
AbhishekNarayan <abhisheknarayan354@gmail.com>
2023-08-14 16:41:51 +0530

    Service Interface

--- backend/springapp/src/main/java/com/bankingapp/service/AccountServiceI.java
+++ backend/springapp/src/main/java/com/bankingapp/service/AccountServiceI.java
@@ -9,15 +9,6 @@ public interface AccountServiceI {
     public List<Account> getAccountFromDatabase();
    
     public Account createAccount(Account account);
-    
-//    public Account getAccountById(int id) throws ResourceNotFoundException  ;
-//    
-//    public ResponseEntity<Account> updateAccount(Integer account_Id, @Valid @RequestBody Account changedAccount )
-//    		throws ResourceNotFoundException ;
-//    
-//    public Map<String, Boolean> deleteAccount(Integer accountId) throws ResourceNotFoundException;
-
-//    void addAccountInDatabase(Account account);
 
 	Account getAccountById(int id) throws ResourceNotFoundException;
 }
