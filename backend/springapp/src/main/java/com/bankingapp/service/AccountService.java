0766913472c7f1485ed078c9def0143091c39af5
AbhishekNarayan <abhisheknarayan354@gmail.com>
2023-08-14 16:32:33 +0530

    Accounts

--- backend/springapp/src/main/java/com/bankingapp/service/AccountService.java
+++ backend/springapp/src/main/java/com/bankingapp/service/AccountService.java
@@ -1,10 +1,18 @@
 package com.bankingapp.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import jakarta.validation.Valid;
+
+import com.bankingapp.exceptions.ResourceNotFoundException;
 import com.bankingapp.models.*;
 import com.bankingapp.repository.*;
 
@@ -21,13 +29,36 @@ public class AccountService implements AccountServiceI {
 	}
 	
 	@Override
-	public void addAccountInDatabase(Account account) {
-		accountRepo.save(account);
+	public Account createAccount(Account account) {
+		return accountRepo.save(account);
 	}
 	
-	public Account getAccountById(int id)   
+	public Account getAccountById(int id) throws ResourceNotFoundException  
 	{  
-		return accountRepo.findById(id).get();  
+		return accountRepo.findById(id).orElseThrow(()-> new ResourceNotFoundException("Account is not available:" + id));  
+	}
+	
+	public ResponseEntity<Account> updateAccount(Integer account_Id, @Valid @RequestBody Account changedAccount )
+	throws ResourceNotFoundException {
+		Account updatedAccount = accountRepo.findById(account_Id).orElseThrow(()-> new ResourceNotFoundException("Account is not available:" + account_Id));
+		
+		updatedAccount.setAccount_id(changedAccount.getAccount_id());
+		updatedAccount.setBalance(changedAccount.getBalance());
+		updatedAccount.setCustomer_id(changedAccount.getCustomer_id());
+		updatedAccount.setTransactionPassword(changedAccount.getTransactionPassword());
+		updatedAccount.setUsername(changedAccount.getUsername());
+		
+		accountRepo.save(updatedAccount);
+		return ResponseEntity.ok(updatedAccount);
+	}
+	
+	public Map<String, Boolean> deleteAccount(Integer accountId) throws ResourceNotFoundException {
+		Account updatedAccount = accountRepo.findById(accountId)
+				.orElseThrow(()-> new ResourceNotFoundException("Account is not avaialble:"+ accountId));
+		accountRepo.delete(updatedAccount);
+		Map<String,Boolean> response  = new HashMap<>();
+		response.put("Account has been Deleted", Boolean.TRUE);
+		return response;
 	}
 
 }
--- backend/springapp/src/main/java/com/bankingapp/service/AccountServiceI.java
+++ backend/springapp/src/main/java/com/bankingapp/service/AccountServiceI.java
@@ -4,7 +4,14 @@ import java.util.List;
 import com.bankingapp.models.*;
 
 public interface AccountServiceI {
-    List<Account> getAccountFromDatabase();
+    public List<Account> getAccountFromDatabase();
     
-    void addAccountInDatabase(Account account);
+    public Account createAccount(Account account);
+    
+//    public Account getAccountById(int id) throws ResourceNotFoundException  ;
+//    
+//    public ResponseEntity<Account> updateAccount(Integer account_Id, @Valid @RequestBody Account changedAccount )
+//    		throws ResourceNotFoundException ;
+//    
+//    public Map<String, Boolean> deleteAccount(Integer accountId) throws ResourceNotFoundException;
 }
