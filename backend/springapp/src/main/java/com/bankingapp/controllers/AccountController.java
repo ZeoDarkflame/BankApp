c4e6dfd72b1652cb393c9bdb4987eeb4512dfef1
ZeoDarkflame <akshanshmalviya@gmail.com>
2023-08-16 10:59:55 +0530

    Disabling security

--- backend/springapp/src/main/java/com/bankingapp/controllers/AccountController.java
+++ backend/springapp/src/main/java/com/bankingapp/controllers/AccountController.java
@@ -39,12 +39,6 @@ public class AccountController {
 		return account;  
 	}  
 	
-	@PostMapping("/add")
-	public String add(@Valid @RequestBody Account account) {
-		accountService.addAccountInDatabase(account);
-		return "Account added successfully";
-	}
-	
 	
 	@PostMapping("/accounts")
     public Account createAccount(@Valid @RequestBody Account newAccount) {
