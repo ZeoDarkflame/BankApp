66a8e20cf33e4d0a17710867e9e33ae54a6ffb82
ZeoDarkflame <akshanshmalviya@gmail.com>
2023-08-23 09:22:11 +0530

    Changes in listservice

--- backend/springapp/src/main/java/com/bankingapp/service/ListService.java
+++ backend/springapp/src/main/java/com/bankingapp/service/ListService.java
@@ -9,8 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.bankingapp.repository.AccountRepository;
 import com.bankingapp.repository.CustomerRepository;
 import com.bankingapp.exceptions.ResourceNotFoundException;
+import com.bankingapp.models.Account;
 import com.bankingapp.models.Customer;
 
 @Service
@@ -18,6 +20,9 @@ public class ListService {
 	
 	@Autowired
 	private CustomerRepository customerrepo;
+	
+	@Autowired
+	private AccountRepository accountrepo;
 	
 	public List<Customer> getCustomerList(){
 		return customerrepo.findAll();
@@ -28,11 +33,27 @@ public class ListService {
 		updatedCustomer.setCustomer_name(downstreamCustomer.getCustomer_name());
 		updatedCustomer.setEmail(downstreamCustomer.getEmail());
 		updatedCustomer.setContact(downstreamCustomer.getContact());
+		customerrepo.save(updatedCustomer);
 		return ResponseEntity.ok(updatedCustomer);
 	}
 	
 	public Customer saveCustomer(Customer newCust) {
 		return customerrepo.save(newCust);
 	}
+
+	public ResponseEntity<Account> switchStatus(int id) throws ResourceNotFoundException {
+		Account switched = accountrepo.findById(id).orElseThrow(()-> new ResourceNotFoundException("Invalid account or account number"));
+		if(switched.getActivity() == 1) {
+			switched.setActivity(0);
+		} else if(switched.getActivity() == 0) {
+			switched.setActivity(1);
+		}
+		
+		accountrepo.save(switched);
+
+		return ResponseEntity.ok(switched);
+	}
+	
+	
 	
 }
